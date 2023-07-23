7a59ee4235f142aa00e9dbf5ffaf7522ea7fc1b3
KlaudiuszB5528 <klaudiusz.biegacz@gmail.com>
2023-07-23 02:15:41 +0200

    project initial config

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css';
+
+import Footer from '@/components/Footer';
+import Navbar from '@/components/Navbar';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dribbleusz',
+  description: 'Dribble Clone to practice Next.js',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <section className="flex-start flex-col paddings mb-16">
+      <h1>Test1</h1>
+      <h1>Bob2</h1>
+      <h1>Bob3</h1>
+    </section>
+  );
+}
